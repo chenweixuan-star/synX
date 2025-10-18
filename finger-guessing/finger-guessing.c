be93adf8bfc413d00ab60dec614944d4019b11e0
chenweixuan-star <3394059798@qq.com>
2025-10-18 18:19:50 +0800

    Revert "feat:增加10秒限时功能"
    This reverts commit d6c79aae8a171791b918550e719c6b4d2edbb946.

--- finger-guessing/finger-guessing.c
+++ finger-guessing/finger-guessing.c
@@ -2,33 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include<Windows.h>
-volatile int timeout_flag=0;
-
-VOID CALLBACK TimeoutHandler(HWND hwnd,UINT uMsg,UINT_PTR idEvent,DWORD dwTime){
-    timeout_flag=1;
-    KillTimer(NULL,idEvent);
-}
 
 int main() {
-    UINT_PTR timer_id;
     srand(time(NULL));
     int user, computer;
-    printf("猜拳游戏:0-石头,1-剪刀,2-布\n ");
-    printf("限时10秒内获胜,否则退出。\n");
-    timer_id=SetTimer(NULL,0,1000*10,TimeoutHandler);
+    printf("猜拳游戏开始！输入 0(剪刀) 1(石头) 2(布): ");
 
-    while(1){
-        if(timeout_flag){
-            printf("\n时间到!游戏结束。\n");
-            break;
-        }
-        printf("请输入你的选择(0/1/2):");
-        if(sacnf("%d",&user)!=1){
-            printf("输入无效!\n");
-            while(getchar()!='\n');
-            continue;
-        }
+    while (1) {
+        scanf("%d", &user);
         if (user < 0 || user > 2) {
             printf("输入无效，请输入 0, 1 或 2\n");
             continue;
@@ -36,24 +17,19 @@ int main() {
 
         computer = rand() % 3;
 
-        printf("电脑出: %d\n", computer);
+        const char* choices[] = {"剪刀", "石头", "布"};
+        printf("你出: %s, 电脑出: %s\n", choices[user], choices[computer]);
 
-        if ((user == 0 && computer == 1) ||
-            (user == 1 && computer == 2) ||
-            (user == 2 && computer == 0)) {
+        if ((user == 0 && computer == 2) ||
+            (user == 1 && computer == 0) ||
+            (user == 2 && computer == 1)) {
             printf("你赢了！游戏结束。\n");
-            KillTimer(NULL,timer_id);
             break;
         } else if (user == computer) {
             printf("平局，再来一次！\n");
         } else {
             printf("你输了，继续！\n");
         }
-        Sleep(100);
-        MSG msg;
-        PeekMessage(&msg,NULL,0,0,PM_REMOVE);
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
     }
 
     return 0;
